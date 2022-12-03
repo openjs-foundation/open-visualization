80144ce2ecdb401b1135dddfee369173dfec3042
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-03 18:48:07 -0500

    Add Prismic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,27 @@
 import type { FC, ReactNode } from 'react';
+import type { LinkProps } from 'next/link';
+import Link from 'next/link';
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '../lib/prismic';
 import '../styles/globals.css';
 
 type RootLayoutProps = {
   children: ReactNode;
 };
 
+const internalLinkComponent = (props: LinkProps) => <Link {...props} />;
+
 const RootLayout: FC<RootLayoutProps> = ({ children }) => (
   <html lang="en">
     <head />
-    <body>{children}</body>
+    <body>
+      <PrismicProvider internalLinkComponent={internalLinkComponent}>
+        <PrismicPreview repositoryName={repositoryName}>
+          {children}
+        </PrismicPreview>
+      </PrismicProvider>
+    </body>
   </html>
 );
 
