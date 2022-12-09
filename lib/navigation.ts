19b5b340e5991e59ea946ad06fc15c7f47e7f1e2
Hayden Bleasel <hello@haydenbleasel.com>
2022-12-08 19:18:41 -0500

    Scaffold Navigation

---
+++ lib/navigation.ts
@@ -0,0 +1,47 @@
+const links = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'About',
+    href: '#about',
+    items: [
+      {
+        label: 'Overview',
+        href: '#about',
+        description: 'What is the OpenJS Foundation?',
+      },
+      {
+        label: 'Charter',
+        href: '#charter',
+        description: 'Goals, principles and the scope of the project.',
+      },
+      {
+        label: 'Code of Conduct',
+        href: '#coc',
+        description: 'How to behave in the community.',
+      },
+      {
+        label: 'TAC',
+        href: '#tac',
+        description: 'Technical Advisory Committee.',
+      },
+      {
+        label: 'Principles',
+        href: '#principles',
+        description: 'The guiding principles of the project.',
+      },
+    ],
+  },
+  {
+    label: 'Projects',
+    href: '#projects',
+  },
+  {
+    label: 'Get Involved',
+    href: '#get-involved',
+  },
+];
+
+export default links;
--- lib/prismic.ts
+++ lib/prismic.ts
@@ -11,12 +11,8 @@ export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint);
 /** @type {prismic.ClientConfig['routes']} **/
 const routes = [
   {
-    type: 'homepage',
+    type: 'home',
     path: '/',
-  },
-  {
-    type: 'page',
-    path: '/:uid',
   },
 ];
 
