f2af34bdef64dedb9f90c7ae57297793033013d6
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-27 09:55:51 -0400

    Remove deprecated aspect-ratio plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
