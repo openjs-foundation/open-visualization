966464d4587f7a24878dcba2f27bb3b4f0bd5ea7
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-30 17:00:36 -0400

    Update callout.tsx

--- components/callout.tsx
+++ components/callout.tsx
@@ -24,41 +24,43 @@ const Callout: FC<CalloutProps> = ({
   ctaLink,
   image,
 }) => (
-  <div className="relative mx-8 my-8 mb-16 max-w-7xl rounded-lg border border-neutral-200 px-4 py-4 dark:border-neutral-700 sm:px-6 sm:py-16 lg:px-8">
-    <div className="grid gap-8 rounded sm:grid-cols-2">
-      <div className="order-2 sm:order-none">
-        <p className="text-sm text-gray-500 dark:text-gray-400">{caption}</p>
-        <h2 className="mb-6 mt-2 text-xl font-bold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-2xl">
-          {title}
-        </h2>
-        <div className="prose prose-sm mx-auto mt-5 text-gray-500 prose-a:text-primary-blue dark:text-gray-400 lg:col-start-1 lg:row-start-1 lg:max-w-none">
-          <PrismicRichText field={description} />
-        </div>
-        {ctaLabel && ctaLink.link_type === 'Web' && (
-          <div className="mt-8">
-            <Link
-              href={(ctaLink as FilledLinkToWebField).url}
-              className={clsx(
-                'inline-flex items-center gap-2 rounded-lg px-4 py-1.5 text-base font-semibold leading-7 ring-1 transition-colors',
-                'text-gray-900 ring-gray-900/10 hover:ring-gray-900/20 dark:text-white dark:ring-white/10'
-              )}
-            >
-              {ctaLabel}
-              <ArrowUpRight className="h-4 w-4" />
-            </Link>
+  <div className="px-8">
+    <div className="relative mx-auto my-8 mb-16 max-w-7xl rounded-lg border border-neutral-200 px-4 py-4 dark:border-neutral-700 sm:px-6 sm:py-16 lg:px-8">
+      <div className="grid gap-8 rounded sm:grid-cols-2">
+        <div className="order-2 sm:order-none">
+          <p className="text-sm text-gray-500 dark:text-gray-400">{caption}</p>
+          <h2 className="mb-6 mt-2 text-xl font-bold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-2xl">
+            {title}
+          </h2>
+          <div className="prose prose-sm mx-auto mt-5 text-gray-500 prose-a:text-primary-blue dark:text-gray-400 lg:col-start-1 lg:row-start-1 lg:max-w-none">
+            <PrismicRichText field={description} />
           </div>
-        )}
-      </div>
-      <div className="h-full w-full">
-        {image.url && (
-          <Image
-            src={image.url}
-            alt={image.alt ?? ''}
-            width={image.dimensions.width}
-            height={image.dimensions.height}
-            className="h-full w-full rounded object-cover object-left"
-          />
-        )}
+          {ctaLabel && ctaLink.link_type === 'Web' && (
+            <div className="mt-8">
+              <Link
+                href={(ctaLink as FilledLinkToWebField).url}
+                className={clsx(
+                  'inline-flex items-center gap-2 rounded-lg px-4 py-1.5 text-base font-semibold leading-7 ring-1 transition-colors',
+                  'text-gray-900 ring-gray-900/10 hover:ring-gray-900/20 dark:text-white dark:ring-white/10'
+                )}
+              >
+                {ctaLabel}
+                <ArrowUpRight className="h-4 w-4" />
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className="h-full w-full">
+          {image.url && (
+            <Image
+              src={image.url}
+              alt={image.alt ?? ''}
+              width={image.dimensions.width}
+              height={image.dimensions.height}
+              className="h-full w-full rounded object-cover object-left"
+            />
+          )}
+        </div>
       </div>
     </div>
   </div>
