7db46bb5cd008e25de008c62868ec75a12d80e79
Ilya Boyandin <ilyabo@gmail.com>
2024-12-05 15:05:49 +0100

    Added ProgressBar for page navigation

--- components/visgl/pages/_app.tsx
+++ components/visgl/pages/_app.tsx
@@ -1,21 +1,21 @@
-"use client"
+'use client';
 
 import Layout from '../components/layout';
 
 import '../styles/fonts.css';
 import '../styles/globals.css';
 import '../styles/layout.css';
-import {Global, ThemeProvider} from '@emotion/react';
-import {globalStyle, theme} from '../components/styling/style';
+import { Global, ThemeProvider } from '@emotion/react';
+import { globalStyle, theme } from '../components/styling/style';
 import React from 'react';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
-function MyApp({children}) {
+function MyApp({ children }) {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
-        <div style={{backgroundColor: 'white'}}>
-          {children}
-        </div>
+      <div style={{ backgroundColor: 'white' }}>{children}</div>
+      <ProgressBar options={{ showSpinner: false }} />
     </ThemeProvider>
   );
 }
