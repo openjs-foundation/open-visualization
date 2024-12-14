6cb0583bb70fbedd05ea5dc63653e296a0bc725b
Ilya Boyandin <ilyabo@gmail.com>
2024-12-14 15:04:54 +0100

    Improve project descriptions readability (#32)
    * Improve project descriptions readability
    
    * Fix: mode icon wasn't visible in light mode
    
    * Fixed AVS image path
    
    * Shorten flowmap description
    
    * Group description styling

--- app/projects/project-card.tsx
+++ app/projects/project-card.tsx
@@ -17,11 +17,11 @@ export const ProjectCard = ({
 }: ProjectEntry) => (
   <a
     href={url}
-    className="group flex flex-row h-16 rounded-md overflow-hidden transition-all duration-300 hover:bg-white/40 dark:hover:bg-gray-800/40"
+    className="group flex flex-row h-auto min-h-[5rem] sm:h-20 sm:hover:h-auto rounded-md overflow-hidden transition-all duration-300 hover:bg-white/40 dark:hover:bg-gray-800/40"
     target="_blank"
     rel="noopener noreferrer"
   >
-    <div className="relative w-24 flex-shrink-0">
+    <div className="relative w-24 min-h-[5rem] sm:h-20 sm:group-hover:h-[inherit] flex-shrink-0">
       <Image
         src={image}
         alt={name}
@@ -36,7 +36,7 @@ export const ProjectCard = ({
       <h3 className="text-sm font-medium text-gray-900 dark:text-gray-100 truncate">
         {name}
       </h3>
-      <p className="text-xs text-gray-500 dark:text-gray-400 line-clamp-1">
+      <p className="text-xs text-gray-500 dark:text-gray-400 sm:line-clamp-2 sm:group-hover:line-clamp-none">
         {description}
       </p>
     </div>
